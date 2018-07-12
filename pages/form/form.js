abd6f532207de6d2895dd60df9b5c65a8fffa3f3
huyanrong <515020842@qq.com>
2018-07-12 09:48:19 +0800

    表单组件 swiper组件

--- pages/form/form.js
+++ pages/form/form.js
@@ -1,8 +1,17 @@
-//获取应用实例
-const app = getApp()
-
 Page({
-    onLoad: function(option){
-      console.log(option.query)
+    data:{
+        switchColor:"red"
+    },
+    switch1Change:function(e){
+        console.log(e);
+    },
+    switch2Change:function(e){
+        console.log(e);
+    },
+    formSubmit: function(e) {
+      console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    },
+    formReset: function() {
+      console.log('form发生了reset事件')
     }
-})
+  })
